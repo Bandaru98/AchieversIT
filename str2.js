4b03724cdd270b6c7df6a71a6e9d15ba6dc08bd1
Bandaru Madhavi <bandarumadhavi681998@gmail.com>
2024-06-17 21:34:48 +0530

    str.js

---
+++ str2.js
@@ -0,0 +1,8 @@
+let string='hello good morning';
+let length=string.length;
+console.log(length);
+
+let str='have a great day     dear';
+str.length= 0;
+console.log(str);//have a great day     dear
+console.log(str.length);//25
