8a847c07089f78d7cb4747084f8c5746f4c631e7
Bandaru Madhavi <bandarumadhavi681998@gmail.com>
2024-07-01 22:06:01 +0530

    Time indication

--- str2.js
+++
@@ -1,8 +0,0 @@
-let string='hello good morning';
-let length=string.length;
-console.log(length);
-
-let str='have a great day     dear';
-str.length= 0;
-console.log(str);//have a great day     dear
-console.log(str.length);//25
